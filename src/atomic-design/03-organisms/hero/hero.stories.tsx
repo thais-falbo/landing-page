92fe0e3b99a66615d90e3e5e8d937de282598aaa
Thaís Falbo <thaisfalbo@outlook.com>
2020-06-16 03:54:48 -0300

    Add knobs to Organisms

--- src/atomic-design/03-organisms/hero/hero.stories.tsx
+++ src/atomic-design/03-organisms/hero/hero.stories.tsx
@@ -2,26 +2,27 @@ import React from "react";
 import { action } from "@storybook/addon-actions";
 import Hero from "./";
 import Capsule from "../../../assets/images/capsule.jpg";
+import { color, array, text } from "@storybook/addon-knobs";
 
 export default {
   title: "Organisms|Hero",
   component: Hero,
 };
 
-const text =
+const textDefault =
   "Lorem ipsum dolor sit amet, consectetuer adipiscing ligula eget dolor.";
 
-const trends = Array(3).fill(text) as [string, string, string];
+const trends = Array(3).fill(textDefault) as [string, string, string];
 
 export const Default = () => (
   <Hero
     image={{ src: Capsule, alt: "Capsule" }}
     cta={{
-      headingText: "Capsule",
-      paragraphText:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie elit at lacus",
-      button: { text: "Click", onClick: action("Click") },
+      headingText: text("Heading", "Capsule"),
+      paragraphText: text("Paragraph", textDefault),
+      button: { text: text("Button", "Click"), onClick: action("Click") },
+      color: color("Color", "#fff"),
     }}
-    trends={trends}
+    trends={array("Trends", trends, "||") as [string, string, string]}
   />
 );
